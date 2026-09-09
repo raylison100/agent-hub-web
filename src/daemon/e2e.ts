7ea221735ca4d987fee2e8e493180f4611ad1d38
Raylison Nunes <12023586+raylison100@users.noreply.github.com>
2026-09-09 19:29:31 -0300

    Interface: cifra importada pelo subcaminho, chips sem quebra e larguras para telas menores

--- src/daemon/e2e.ts
+++ src/daemon/e2e.ts
@@ -1,1 +1,1 @@
-export { deriveE2eKey, isSealed, openFrame as open, sealFrame as seal, type SealedFrame } from '@agent-hub/core'
+export { deriveE2eKey, isSealed, open, seal, type SealedFrame } from '@agent-hub/core/e2e'
